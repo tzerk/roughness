53201a6922f8f2b75be0cf23c1a76b2a518e1d1b
C. Burow <C. Burow>
2016-07-20 14:52:53 +0100

    ACL: make sure that first nearest value is found + plot

--- R/calc_CorrelationLength.R
+++ R/calc_CorrelationLength.R
@@ -18,14 +18,25 @@ calc_CorrelationLength <- function(data, plot = TRUE) {
   
   colnames(data) <- c("x", "y")
   
-  acf <- acf(data$y, lag.max = length(data$y) / 4, na.action = na.pass)
+  acf <- acf(data$y, lag.max = length(data$y) / 4, na.action = na.pass, plot = plot)$acf
   
   euler <- 1 / exp(1)
   
-  
-  
-  l.index <- which.min(abs(acf$acf - euler))
+  # we want to find the very first occurence of euler; we discard all values
+  # after the first negative value and onward
+  neg <- which(acf < 0)[1]
+  if (!is.na(neg))
+    acf <- acf[1:neg] 
+    
+  # determine the closest index and value to 1/e
+  l.index <- which.min(abs(acf - euler))
   l <- data$x[l.index]
+  
+  # add this point to the plot for visual confirmation
+  if (plot) {
+    points(l.index, euler, col = "red", pch = 16)
+    legend("topright", legend = "Value closest to 1/e", pch = 16, col = "red")
+  }
   
   return(l)
 }
